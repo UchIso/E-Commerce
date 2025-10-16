e88ed1ddc7554663f74be7e64fdf05fc4264da85
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-10-16 22:00:35 +0400

    OP

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { BiLabel } from 'react-icons/bi';
-import { CiDark , CiLight } from "react-icons/ci";
+import { CiDark , CiLight , CiSearch  } from "react-icons/ci";
 import { SiVitest } from "react-icons/si";
 
 function Header() {
@@ -10,6 +10,7 @@ function Header() {
   const [Theme,SetTheme] = useState({
     Container:"#1A1A1A",
     Header:"#5F5F5F",
+    Shadow:"#C7C8C2",
     Text:"#E6E7E1",
   })
   
@@ -18,11 +19,13 @@ function Header() {
       Body.style.background = "#2F2F2F"
       Theme.Container = "#1A1A1A"
       Theme.Header = "#5F5F5F"
+      Theme.Shadow = "#C7C8C2"
       Theme.Text = "#E6E7E1"
     }else{
       Body.style.background = "#E7E8E2"
       Theme.Container = "#3F3F3F"
       Theme.Header = "#C7C8C2"
+      Theme.Shadow = "#5F5F5F"
       Theme.Text = "#0A0A0A"
     }    
     SetThemeCh(!ThemeCh)
@@ -34,8 +37,16 @@ function Header() {
             <SiVitest className='text-5xl' />
             <h2>Fake React E-commerce</h2>
         </div>
-        <div className='Tehem'>
-          {ThemeCh? <CiDark onClick={ChangeTheme} className="cursor-pointer" /> : <CiLight onClick={ChangeTheme} className="cursor-pointer" />}
+        <div className='HeaderRight'>
+
+          <div className='Searchs'>
+            <CiSearch className='text-3xl' />
+            <input className='SearchInput' type="text" />
+          </div>
+          
+          <div style={{boxShadow: "0px 0px 10px"+Theme.Shadow}} className='Tehem'>
+            {ThemeCh? <CiDark onClick={ChangeTheme} className="cursor-pointer" /> : <CiLight onClick={ChangeTheme} className="cursor-pointer" />}
+          </div>
         </div>
     </div>
   )
